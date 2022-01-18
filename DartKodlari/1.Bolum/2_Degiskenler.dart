be74246c5f501577bc5f2f613abc3d2b46353800
EmrullahKutlar <62060214+EmrullahKutlar@users.noreply.github.com>
2022-01-19 01:38:36 +0300

    ilk commit

---
+++ DartKodlari/1.Bolum/2_Degiskenler.dart
@@ -0,0 +1,5 @@
+void main(List<String> args) {
+ /* num hem double hemde int veri saklayabilir. yada double ve int kullanabiliriz */
+  /* digerleri standart degiskenler string ve boolen  */
+  /* var herşey olabilir string boolen int double. javaScripttekki gibi */
+}
---
+++ DartKodlari/1.Bolum/3_Num_Boolen.dart
@@ -0,0 +1,29 @@
+void main(List<String> args) {
+  int yas = 34;
+  print(yas);
+
+  yas = 44;
+  print(yas);
+
+  num yil = 1985;
+  print(yil);
+
+  double sayi = 55;
+  print(sayi);
+
+  int kilo = 85.6.toInt();
+  print(kilo);
+
+  var sayi2 = 56;
+
+  /* var'da degiskene bir deger atadıktan sonra o degiskenin değeri sadece
+  aynı turden degerle degisebilir. Mesela burada sayi2 string yada double bir 
+  deger alamaz */
+
+  /*degisken yaratırken deger atamadan kullanılmıyor mutlaka bir degeri olmalı
+  eger illaki null birakmak istiyorsak degiskenin yanına "?" bırakılır ,
+  assagidaki gibi*/
+
+  int? bos = null;
+  
+}
